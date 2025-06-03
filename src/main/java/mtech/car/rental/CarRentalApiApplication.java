b142cea86a9032ed0020e40812f66fd5c9369bab
Murilo Castro <murilo.65@gmail.com>
2025-06-03 20:53:36 -0300

    feat: Creating Car Rental Api Project

---
+++ src/main/java/mtech/car/rental/CarRentalApiApplication.java
@@ -0,0 +1,13 @@
+package mtech.car.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalApiApplication.class, args);
+	}
+
+}
